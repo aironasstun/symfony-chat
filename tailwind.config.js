b8cc3746cf462e276597fdf12b6477070fa5eae3
aironasstun <stunzenas.aironas@gmail.com>
2024-04-16 22:55:35 +0300

    Addition of theme palette colors, changes to templates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,45 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   content: [
     "./assets/**/*.js",
     "./templates/**/*.html.twig",
   ],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
+      'text': {
+        light: '#191f14',
+        dark: '#e5ebe0',
+      },
+      'background': {
+        light: '#f2f5ef',
+        dark: '#0d100a',
+      },
+      'primary': {
+        light: '#445535',
+        dark: '#b9caaa',
+      },
+      'secondary': {
+        light: '#98b0bd',
+        dark: '#425a67',
+      },
+      'accent': {
+        light: '#585c89',
+        dark: '#767aa7',
+      },
+    },
     extend: {
       spacing: {
         'chat': '70vh',
-      },
-      backgroundImage: {
-        'jumbotron-image': "url('../images/background.jpg')",
       },
     },
   },
